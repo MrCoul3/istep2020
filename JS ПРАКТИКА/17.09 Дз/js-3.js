1600645cdc9aa429ed695db9447fbb3e1574794e
MrCoul <mr.coul@inbox.ru>
2020-09-21 14:36:30 +0300

    21.09

--- JS ПРАКТИКА/17.09 Дз/js-3.js
+++ JS ПРАКТИКА/17.09 Дз/js-3.js
@@ -11,56 +11,65 @@
 // Если пользователь соглашается с обменом, то выводим на экран сообщение “Мы поменяли {кол-во денег, которые передал клиент} {валюта, которую передал пользователь} на {кол-во денег, которые вернул обменник} {валюта, которую вернул обменник}.
 //  В противном случае выводим случае выводим сообщение “Увидимся в следующий раз”
 
-const $_RATE = 74.93;
-RUB_RATE = 0.013;
-let operationInfo;
-let agreement;
-let sumOfCash;
-
-let currencyType = prompt("Какую валюту вы хотите обменять - USD или RUB?", "USD");
-console.log(currencyType);
-sumOfCash = prompt("Введите сумму, необходимую для обмена", "100");
-exchanger();
 
 
 function exchanger() {
+    currencyType = prompt("Какую валюту вы хотите обменять - USD или RUB?", "USD");
+
     if (currencyType == "usd" || currencyType == "USD") {
         reverseType = "RUB";
+        sumOfCash = prompt("Введите сумму, необходимую для обмена", "100");
         operationInfo = (sumOfCash * $_RATE).toFixed(2);
-        console.log(sumOfCash * $_RATE);
         alert(`Вы получите ${operationInfo} RUB`);
         agreement = confirm(`Вы согласны обменять ${sumOfCash} USD на ${operationInfo} RUB?`);
     }
     else if (currencyType == "rub" || currencyType == "RUB") {
         reverseType = "USD";
+        sumOfCash = prompt("Введите сумму, необходимую для обмена", "100");
         operationInfo = (sumOfCash * RUB_RATE).toFixed(2);
         alert(`Вы получите ${operationInfo} USD`);
         agreement = confirm(`Вы согласны обменять ${sumOfCash} RUB на ${operationInfo} USD?`);
     }
     else {
-        alert("Вы неверно ввели валюту! Нужно ввести USD или RUB")
-        
+        alert("Вы неверно ввели валюту! Нужно ввести USD или RUB");
+        exchanger();
     }
-
+    result();
 }
-
-result();
 
 function result() {
     if (agreement == true) {
-        alert(`Мы поменяли ${sumOfCash} ${currencyType} на ${operationInfo} ${reverseType}`)
+        alert(`Мы поменяли ${sumOfCash} ${currencyType} на ${operationInfo} ${reverseType}`);
+        anotherOneChange();
     }
     else {
         alert("Увидимся в следующий раз!")
     }
 }
 
-// let anotherOne = confirm("Хотите выполнить еще одну операцию?");
-// anotherOneChange();
+function anotherOneChange() {
+    anotherOne = confirm("Хотите выполнить еще одну операцию?");
+    if (anotherOne == true) {
+        exchanger();
+    }
+    else {
+        alert("До следующего раза!")
+    }
+}
 
-// function anotherOneChange() {
-//     if (anotherOne == true) {
 
-//     }
-// }
+
+const $_RATE = 74.93;
+RUB_RATE = 0.013;
+let operationInfo;
+let agreement;
+let sumOfCash;
+let currencyType;
+let anotherOne;
+
+exchanger();
+
+
+
+
 
