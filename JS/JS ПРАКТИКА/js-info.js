710113e42916d4650764c1f8eef77b5f8f6fd0e3
MrCoul <mr.coul@inbox.ru>
2020-09-23 15:06:18 +0300

    23.09 15:06

--- JS/JS ПРАКТИКА/js-info.js
+++ JS/JS ПРАКТИКА/js-info.js
@@ -18,13 +18,13 @@
 // Операторы сравнения 
 
 
-5 > 4 //true
-"ананас" > "яблоко" //false
-"2" > "12" //true 
-undefined == null // true
-undefined === null //false
-null == "\n0\n" // false
-null === +"\n0\n" // false
+// 5 > 4 //true
+// "ананас" > "яблоко" //false
+// "2" > "12" //true 
+// undefined == null // true
+// undefined === null //false
+// null == "\n0\n" // false
+// null === +"\n0\n" // false
 
 
 //  ####   УСЛОВНЫЕ ОПЕРАТОРЫ  ##### \\
